324d713bf183e11a45a5ababca79dc77da104c1e
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-12-21 10:24:18 -0800

    create UpdateDescription method

--- ToDoList/Models/Task.cs
+++ ToDoList/Models/Task.cs
@@ -166,7 +166,29 @@ namespace ToDoList.Models
 
         public void UpdateDescription(string newDescription)
         {
-          
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"UPDATE tasks SET description = @newDescription WHERE id = @searchId;";
+
+            MySqlParameter searchId = new MySqlParameter();
+            searchId.ParameterName = "@searchId";
+            searchId.Value = _id;
+            cmd.Parameters.Add(searchId);
+
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@newDescription";
+            description.Value = newDescription;
+            cmd.Parameters.Add(description);
+
+            cmd.ExecuteNonQuery();
+            _description = newDescription;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
         }
     }
 }
